73c8afbce14ccab778e08e9331a95601835ff30c
Shota Nakagawa <NakaSho@NakaSho-MacBook-Pro.local>
2020-01-19 21:42:43 +0900

    aaa

---
+++ practice_5.py
@@ -0,0 +1,16 @@
+import autograd.numpy as np
+from autograd import grad
+
+def tanh(x):
+    y = np.exp(-2 * x)
+    return (1.0 - y) / (1.0 + y)
+
+from autograd import elementwise_grad as egrad
+import matplotlib.pyplot as plt
+x = np.linspace(-7, 7, 200)
+plt.plot(x, tanh(x),
+         x, egrad(tanh)(x),
+         x, np.sin(x),
+         x, np.cos(x))
+
+plt.show()
